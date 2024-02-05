aeca18b05f3af639807994b97a8497a24a015f7e
Mangesh Phadte <95576296+VOID-001@users.noreply.github.com>
2024-02-05 16:19:28 +0000

    Initiate commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
 const express = require("express");
-
 const app = express();
+const http = require("http");
+const cors = require("cors");
+const {}=require("socket.io")
 
-app.listen(3000,()=>{
-    console.log("server listing on port 3000 http://localhost:3000");
+app.use(cors());
+const server =http.createServer(app);
+
+const io = new server(server, {
+    cors: {
+        origon: " htttp://localhost:3000",
+        method: ["GET","POST"]
+    }
+});
+
+io.on("connection", ()=>{
+    console.log(socket.id);
+socket.on("disconnect",()=>
+{
+    console.log("User Disconnected",socket.id)
+});
+
+});
+
+server.listen(3001, () => {
+    console.timeLog("Server Running")
 })
+
