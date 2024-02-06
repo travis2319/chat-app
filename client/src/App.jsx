7a8033e460b7d440f1617a1d4c766f3340b8a0ac
travis fernandes <41868352+travis2319@users.noreply.github.com>
2024-02-06 06:19:48 +0000

    as

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,6 @@ import io from 'socket.io-client'
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
       <>hello</>
   )
